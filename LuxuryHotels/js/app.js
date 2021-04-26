948f5a617406c4f545e3e10c19e74c32647ced2c
Anton Kozhukhar <anton.kozhukhar@gmail.com>
2021-04-26 15:44:35 +0300

    Fixed bugs

--- LuxuryHotels/js/app.js
+++ LuxuryHotels/js/app.js
@@ -1,13 +1,12 @@
 //Active menu
-
 let menuLinks = document.querySelectorAll('.header__link');
 
 for (let i = 0; i < menuLinks.length; i++) {
-  if (document.location.href === menuLinks[i].href) {
-    menuLinks[i].classList.add('_active');
-  }
+	if (document.location.href === menuLinks[i].href) {
+		menuLinks[i].classList.add('_active');
+	}
 }
-console.log(location.pathname.indexOf(menuLinks[1].text));
+
 //Burger-menu
 let burger = document.querySelector('.header__burger');
 let menu = document.querySelector('.header__list');
@@ -24,11 +23,10 @@ burger.addEventListener('click', function () {
 });
 
 //Srcoll
-
 if (
-	location.pathname === '/home.html' ||
-	location.pathname === '/facilities.html' ||
-	location.pathname === '/rooms.html'
+	location.pathname.includes('/home.html') ||
+	location.pathname.includes('/facilities.html') ||
+	location.pathname.includes('/rooms.html')
 ) {
 	let scroll = document.querySelector('.luxury__scroll-btn[data-goto]');
 
@@ -64,65 +62,87 @@ new Swiper('.rooms__slider', {
 	speed: 600,
 });
 
-//Validation
+//Dropdown
+const view = document.querySelectorAll('.rooms__details');
+const dropdown = document.querySelectorAll('.rooms__details-list');
 
-let regEmail = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+for (let i = 0; i < view.length; i++) {
+  view[i].addEventListener("click", function() {
+    view[i].classList.toggle('_active');
+    dropdown[i].classList.toggle('_active');
+  });
+}
+
+
+//Validation
+const regEmail = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
 
 const footerForm = document.forms.footerForm;
 const footerInput = footerForm.elements.footerInput;
+const footerErrorMessage = document.querySelector('.error-message');
+
 
 footerForm.addEventListener('submit', function (e) {
-  e.preventDefault();
-
+	e.preventDefault();
   formRemoveError(footerForm);
+  removeErrorMessage(footerErrorMessage)
 
-  if (emailTest(footerInput)) {
+	if (emailTest(footerInput)) {
     formAddError(footerForm);
-  }
-
-  if (footerInput.value === '') {
+    addErrorMessage(footerErrorMessage)
+	}
+	if (footerInput.value === '') {
     formAddError(footerForm);
-  }
+    addErrorMessage(footerErrorMessage)
+	}
 });
 
-
-if (location.pathname === '/contact-us.html') {
-  const form = document.forms.form;
-
+const form = document.forms.form;
+if (location.pathname.includes('/contact-us.html')) {
 	form.addEventListener('submit', formSend);
 
 	function formSend(e) {
 		e.preventDefault();
-    formValidate(form);
+		formValidate();
 	}
+}
+function formValidate() {
+	const formReq = document.querySelectorAll('._input');
+  const errorMessages = document.querySelectorAll('.contact__message');
 
-	function formValidate(form) {
-		let formReq = document.querySelectorAll('._req');
+	for (let i = 0; i < formReq.length; i++) {
+		const input = formReq[i];
+    const message = errorMessages[i];
 
-		for (let i = 0; i < formReq.length; i++) {
-			const input = formReq[i];
-			formRemoveError(input);
+		formRemoveError(input);
+    removeErrorMessage(message);
 
-			if (input.classList.contains('_email')) {
-				if (emailTest(input)) {
-					formAddError(input);
-				}
-			} else {
-				if (input.value === '') {
-					formAddError(input);
-				}
+		if (input.name === 'email') {
+			if (emailTest(input)) {
+        formAddError(input);
+        addErrorMessage(message);
+			}
+    } else {
+			if (input.value === '') {
+        formAddError(input);
+        addErrorMessage(message)
 			}
 		}
 	}
+}
+function formAddError(input) {
+	input.classList.add('_error');
+}
+function formRemoveError(input) {
+	input.classList.remove('_error');
+}
+function emailTest(input) {
+	return !regEmail.test(input.value);
+}
 
-	function formAddError(input) {
-		input.classList.add('_error');
-	}
-	function formRemoveError(input) {
-		input.classList.remove('_error');
-	}
-
-	function emailTest(input) {
-		return !regEmail.test(input.value);
-	}
+function addErrorMessage(message) {
+  message.classList.add('_active');
+}
+function removeErrorMessage(message) {
+  message.classList.remove('_active');
 }
